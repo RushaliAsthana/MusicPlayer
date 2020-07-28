8f18cc5853efae6f6cf5ace79ab08f867302e700
Rushali Asthana <rushaliasthana@gmail.com>
2020-07-28 20:00:19 +0530

    Updates

---
+++ precache-manifest.580a94840bc061ee68d52b17ade1baf5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11fe9c3567e1a2cc36f24240ab2b809",
+    "url": "/MusicPlayer/index.html"
+  },
+  {
+    "revision": "8fdeb29cbdfefc064d92",
+    "url": "/MusicPlayer/static/css/main.0bb4caf8.chunk.css"
+  },
+  {
+    "revision": "c3c4ffd004c57bd1e2e8",
+    "url": "/MusicPlayer/static/js/2.0a1e4f3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MusicPlayer/static/js/2.0a1e4f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdeb29cbdfefc064d92",
+    "url": "/MusicPlayer/static/js/main.f835abde.chunk.js"
+  },
+  {
+    "revision": "6a20decdba2ce5ce5816",
+    "url": "/MusicPlayer/static/js/runtime-main.8bb7dbc5.js"
+  },
+  {
+    "revision": "0467922e0817d13a434e20c3be5383b1",
+    "url": "/MusicPlayer/static/media/DilIbaadat.0467922e.mp3"
+  },
+  {
+    "revision": "f5232b01c56264a55cd3595c45196005",
+    "url": "/MusicPlayer/static/media/DilIbadat.f5232b01.jpg"
+  },
+  {
+    "revision": "23bdc8fde7beec3c3c12a072614cab11",
+    "url": "/MusicPlayer/static/media/KMPH.23bdc8fd.mp3"
+  },
+  {
+    "revision": "f9304908fce967fde9464246735f82bf",
+    "url": "/MusicPlayer/static/media/KMPH.f9304908.jpg"
+  },
+  {
+    "revision": "881946ba5fe19310b43c8e2fdb147222",
+    "url": "/MusicPlayer/static/media/Kamli.881946ba.mp3"
+  },
+  {
+    "revision": "e25d677de0713533e6b5e52e87923c8b",
+    "url": "/MusicPlayer/static/media/LKLP.e25d677d.jpg"
+  },
+  {
+    "revision": "19891678b7facf5acc6ffe68c326f180",
+    "url": "/MusicPlayer/static/media/LabonKo.19891678.mp3"
+  },
+  {
+    "revision": "2a4672a16ff95b7d39a8642a42a08d0b",
+    "url": "/MusicPlayer/static/media/Muqabla.2a4672a1.mp3"
+  },
+  {
+    "revision": "a0582c5a697ed3126ab79af1071ebeaa",
+    "url": "/MusicPlayer/static/media/StreetDancer.a0582c5a.jpg"
+  },
+  {
+    "revision": "87423c72df60a3b74585dd26daa75d99",
+    "url": "/MusicPlayer/static/media/Tum Se Hi.87423c72.mp3"
+  },
+  {
+    "revision": "26dda61f3f5aa0521ecdf1b4639cf346",
+    "url": "/MusicPlayer/static/media/kamli.26dda61f.jpg"
+  },
+  {
+    "revision": "c68547da19a56eda6a458c9effe26508",
+    "url": "/MusicPlayer/static/media/logo.c68547da.png"
+  },
+  {
+    "revision": "508d790ac1e41c9cd25eaab5b6f42d9e",
+    "url": "/MusicPlayer/static/media/sidebarheader.508d790a.png"
+  },
+  {
+    "revision": "398647358e7efa8375d7767948e9aded",
+    "url": "/MusicPlayer/static/media/tumsehi.39864735.jpg"
+  }
+]);
